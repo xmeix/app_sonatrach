dfb482784357daf95900b86f857596a09f783a36
xmeix <lamiaboualouache@gmail.com>
2022-08-13 02:06:42 +0100

    fixedForm

--- lib/screens/new_demande.dart
+++ lib/screens/new_demande.dart
@@ -1,3 +1,5 @@
+import 'package:app_sonatrach/addons/data.dart';
+import 'package:app_sonatrach/models/demande_bon_sortie.dart';
 import 'package:app_sonatrach/widgets/navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -13,7 +15,8 @@ class NewDemandeScreen extends StatefulWidget {
 
 class _NewDemandeScreenState extends State<NewDemandeScreen> {
   final motif = TextEditingController();
-  TimeOfDay time = TimeOfDay.now();
+  TimeOfDay timeDeb = TimeOfDay.now();
+  TimeOfDay timeFin = TimeOfDay.now();
 
   Widget buildTextFormField(String title, TextEditingController controller) {
     return TextField(
@@ -51,7 +54,8 @@ class _NewDemandeScreenState extends State<NewDemandeScreen> {
         ));
   }
 
-  Widget buildTimePicker(String desc, String heure, String min) {
+  Widget buildTimePicker(
+      String desc, String heure, String min, TimeOfDay time) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
@@ -72,7 +76,20 @@ class _NewDemandeScreenState extends State<NewDemandeScreen> {
 
   Widget buildAddDemandeButton(String title) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        final newDem = DemandeSortie(
+            id: (listeDemSortie.length + 1).toString(),
+            heureDeb:
+                '${timeDeb.hour.toString().padLeft(2, "0")}:${timeDeb.minute.toString().padLeft(2, "0")}',
+            heureFin:
+                '${timeFin.hour.toString().padLeft(2, "0")}:${timeFin.minute.toString().padLeft(2, "0")}',
+            motif: motif.text,
+            etat: "",
+            senderName: "TestUser");
+        setState(() {
+          listeDemSortie.add(newDem);
+        });
+      },
       child: Text(title),
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all(Colors.grey[500]),
@@ -82,10 +99,10 @@ class _NewDemandeScreenState extends State<NewDemandeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final heureDeb = time.hour.toString().padLeft(2, "0");
-    final heureFin = time.hour.toString().padLeft(2, "0");
-    final minDeb = time.minute.toString().padLeft(2, "0");
-    final minFin = time.minute.toString().padLeft(2, "0");
+    final heureDeb = timeDeb.hour.toString().padLeft(2, "0");
+    final heureFin = timeFin.hour.toString().padLeft(2, "0");
+    final minDeb = timeDeb.minute.toString().padLeft(2, "0");
+    final minFin = timeFin.minute.toString().padLeft(2, "0");
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(40),
@@ -94,8 +111,10 @@ class _NewDemandeScreenState extends State<NewDemandeScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             buildTextFormField("motif", motif),
-            buildTimePicker("Selectionner Heure Debut", heureDeb, minDeb),
-            buildTimePicker("Selectionner Heure Fin", heureFin, minFin),
+            buildTimePicker(
+                "Selectionner Heure Debut", heureDeb, minDeb, timeDeb),
+            buildTimePicker(
+                "Selectionner Heure Fin", heureFin, minFin, timeFin),
             buildAddDemandeButton('Ajouter demande de sortie'),
           ],
         ),
